bed525ed9f38fee028dd124b6cede72d31b4d34d
stens0n <astenson010@gmail.com>
2022-08-09 18:56:14 -0400

    Button Changes

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const CTA = () => {
   return (
     <div className="cta">
-        <a href='#contact'>Let's Talk</a>
+        <a href='#contact'
+           className="btn btn-primary">Let's Talk</a>
     </div>
   )
 }
