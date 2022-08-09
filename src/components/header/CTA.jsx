14837fee012d32259a7f5cc2f6cf656f3f4e76c5
stens0n <astenson010@gmail.com>
2022-08-09 18:19:40 -0400

    changes

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CTA = () => {
+  return (
+    <div className="cta">
+        <a href='#contact'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Anthony Stenson</h1>
+        <h5 className="text-light">Self Taught Fullstack Developer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
