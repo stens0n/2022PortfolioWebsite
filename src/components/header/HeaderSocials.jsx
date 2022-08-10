b0f60fd5217c32e277d4577213dfeeff5160578c
stens0n <astenson010@gmail.com>
2022-08-10 12:45:44 -0400

    header updates

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,6 @@ const HeaderSocials = () => {
     <div className="header__socials">
         <a href="https://linkedin.com" target="_blank"> <BsLinkedin/> </a>
         <a href="https://github.com" target="_blank"> <FaGithub /> </a>
-        <a href="https://dribble.com" target="_blank"></a>
     </div>
   )
 }
