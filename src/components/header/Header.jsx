0752fb9ce392969ac1e58411d43f281187da571f
stens0n <astenson010@gmail.com>
2022-08-10 11:53:38 -0400

    header updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './header.css';
-import CTA from './CTA'
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocial from './HeaderSocials';
 
 const Header = () => {
   return (
@@ -10,6 +12,13 @@ const Header = () => {
         <h1>Anthony Stenson</h1>
         <h5 className="text-light">Self Taught Fullstack Developer</h5>
         <CTA />
+        <HeaderSocial />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
   )
